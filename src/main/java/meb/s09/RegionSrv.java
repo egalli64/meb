dc378734ab30140d37ca3fcd03e5ab653e5e79a9
Manny <egalli64@gmail.com>
2020-06-05 10:43:19 +0200

    URL fix

--- src/main/java/meb/s09/RegionSrv.java
+++ src/main/java/meb/s09/RegionSrv.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/s08/region")
+@WebServlet("/s09/region")
 public class RegionSrv extends HttpServlet {
     @EJB
     RegionBean service;
